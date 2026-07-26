6d7e40872c004f28da0760eba24cbf72ad51286c
Erik <erikolsson0@proton.me>
2026-07-26 10:00:19 +0200

    web socket fix

--- server/client.go
+++ server/client.go
@@ -27,7 +27,7 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all origins for development
+		return r.Header.Get("Origin") == "https://222erik.github.io"
 	},
 }
 
